5792ca9222bc0e8870f7102e3d20363a6677d73b
santiagosatama393 <santiagosatama393@gmail.com>
2026-06-22 08:59:07 -0500

    frits

--- README.md
+++ README.md
@@ -1,18 +1,53 @@
-## Getting Started
+# Práctica: Estructuras no lineales en Java
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+## Datos del Estudiante
+- **Nombre:** [Santiago Satama]
+- **Curso:** [Estructura de Datos Gpo #1]
+- **Fecha:** [17/06/2026]
 
-## Folder Structure
+---
 
-The workspace contains two folders by default, where:
+## 1. Implementación de Estructuras no lineales
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+**Fecha:** [19/06/2026]
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
+**Descripción:** Se realizó una práctica trabajando con nodos y árboles binarios. En el archivo IntTree.java se implementaron, usando recursividad, los métodos de recorrido PreOrder(primero se visita la raíz, luego el subárbol izquierdo y después el derecho), PosOrder(primero se visita los subárboles y al final la raíz), InOrder(como lo dice su nombre este hace un recorrido en orden ya sea ascendente y descendente), así como los métodos Height(calcular la altura del árbol) y Weight(calcular cantidad de nodos "peso del arbol"). Todos los métodos fueron resueltos aplicando un enfoque recursivo, reforzando la comprensión de cómo se procesan estructuras jerárquicas.
 
-## Dependency Management
+## Implementacion del codigo 
+![alt text](image.png)
+### PreOrden IntTree
+![alt text](image-1.png)
+### PosOrden IntTree
+![alt text](image-2.png)
+### InOrden IntTree
+![alt text](image-3.png)
+### Height IntTree
+![alt text](image-4.png)
+### Weight IntTree
+![alt text](image-5.png)
+### App.java IntTree
+![alt text](image-6.png)
 
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+## Salida de consola
+![alt text](image-7.png)
+
+---
+**Descripción:** En la siguiente práctica se trabajó con una clase genérica BinaryTree<> extends Comparable<>, la cual permite almacenar cualquier objeto que implemente Comparable. Se creó la clase Persona con los atributos nombre y edad, haciendo uso de implements Comparable<> y sobrescribiendo el método compareTo para definir el criterio de orden. Luego, en BinaryTree, se implementó los mismos recorridos de la practica anterior.
+## Implementacion del codigo 
+![alt text](assets/image-8.png)
+### PreOrden BinaryTree<>
+![alt text](assets/image-9.png)
+### PosOrden BinaryTree<>
+![alt text](assets/image-10.png)
+### InOrden BinaryTree<>
+![alt text](assets/image-11.png)
+### Height BinaryTree<>
+![alt text](assets/image-12.png)
+### Weight BinaryTree<>
+![alt text](assets/image-13.png)
+### App.java BinaryTree<>
+![alt text](assets/image-14.png)
+
+## Salida de consola
+![alt text](assets/image-15.png)
--- src/App.java
+++ src/App.java
@@ -1,60 +1,66 @@
 import models.Persona;
-import structures.node.Node;
+// import structures.node.Node;
 import structures.trees.BinaryTree;
-import structures.trees.Ejercicio1;
-import structures.trees.Ejercicio2;
+// import structures.trees.Ejercicio1;
+// import structures.trees.Ejercicio2;
 import structures.trees.IntTree;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // runIntTree();
-        // runBinaryTree();
-        runEjercicios();
+        runIntTree();
+        runBinaryTree();
+        // runEjercicios();
         
     }
 
-    private static void runEjercicios() {
-        Ejercicio1 ejercicio1 = new Ejercicio1();
-        int [] numeros = new int[] {5, 3, 7, 2, 4, 6, 8};
-        ejercicio1.insert(numeros);
+    // private static void runEjercicios() {
+    //     Ejercicio1 ejercicio1 = new Ejercicio1();
+    //     int [] numeros = new int[] {5, 3, 7, 2, 4, 6, 8};
+    //     ejercicio1.insert(numeros);
 
-        Ejercicio2 ejercicio2 = new Ejercicio2();
-        int[] numeros2 = new int[] {5, 3, 7, 2, 4, 6, 8};
-        BinaryTree<Integer> tree = new BinaryTree<>();
-        for (int numero2: numeros2)
-            tree.add(numero2);
-        Node<Integer> root = tree.getRoot();
-        ejercicio2.inverTree(root);
+    //     Ejercicio2 ejercicio2 = new Ejercicio2();
+    //     int[] numeros2 = new int[] {5, 3, 7, 2, 4, 6, 8};
+    //     BinaryTree<Integer> tree = new BinaryTree<>();
+    //     for (int numero2: numeros2)
+    //         tree.add(numero2);
+    //     Node<Integer> root = tree.getRoot();
+    //     ejercicio2.inverTree(root);
 
-    }
-
-    // private static void runBinaryTree() {
-    //     BinaryTree<String> arbolStrings = new BinaryTree<>();
-    //     BinaryTree<Persona> arbolPersonas = new BinaryTree<>();
-    //     arbolPersonas.add(new Persona("Pablo", 30));
-    //     arbolPersonas.add(new Persona("Ana", 25));
-    //     arbolPersonas.add(new Persona("Luis", 35));
-    //     arbolPersonas.add(new Persona("Maria", 28));
-        
     // }
 
-    // private static void runIntTree() {
-    //     IntTree arbolNumeros = new IntTree();
-    //     arbolNumeros.add(50);
-    //     arbolNumeros.add(10);
-    //     arbolNumeros.add(30);
-    //     arbolNumeros.add(60);
-    //     arbolNumeros.add(70);
-    //     arbolNumeros.add(55);
+    private static void runBinaryTree() {
+        // BinaryTree<String> arbolStrings = new BinaryTree<>();
+        BinaryTree<Persona> arbolPersonas = new BinaryTree<>();
+        arbolPersonas.add(new Persona("Pablo", 30));
+        arbolPersonas.add(new Persona("Ana", 25));
+        arbolPersonas.add(new Persona("Luis", 35));
+        arbolPersonas.add(new Persona("Maria", 28));
+        
+    }
 
-    //     System.out.println("-----Preorden-----");
-    //     arbolNumeros.preOrden();
-    //     System.out.println();
-    //     System.out.println("-----PosOrden-----");
-    //     arbolNumeros.posOrden();
-    //     System.out.println();
-    //     System.out.println("-----InOrden-----");
-    //     arbolNumeros.inOrden();
+    private static void runIntTree() {
+        IntTree arbolNumeros = new IntTree();
+        arbolNumeros.add(50);
+        arbolNumeros.add(10);
+        arbolNumeros.add(30);
+        arbolNumeros.add(60);
+        arbolNumeros.add(70);
+        arbolNumeros.add(55);
+
+        System.out.println("-----Preorden-----");
+        arbolNumeros.preOrden();
+        System.out.println();
+        System.out.println("-----PosOrden-----");
+        arbolNumeros.posOrden();
+        System.out.println();
+        System.out.println("-----InOrden-----");
+        arbolNumeros.inOrden();
+        System.out.println();
+        System.out.println("-----Height-----");
+        System.out.println(arbolNumeros.getHeight());
+        System.out.println();
+        System.out.println("-----Weight-----");
+        System.out.println(arbolNumeros.getWeight());
 
         // Node<Integer> node1 = new Node(50);
         // Node<Integer> node2 = new Node(10);
@@ -70,7 +76,7 @@ public class App {
 
 
 
-    // }
+    }
 }
 
 
